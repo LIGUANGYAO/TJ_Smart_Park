9548f5a3dacaccd642c33039d3d794c248c0d596
xpwsg <xpwsgg@163.com>
2019-01-19 10:53:31 +0800

    账单管理之收费项目完成

---
+++ apps/cost_management/model/CostCategoryList.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xpwsg
+ * Date: 2019/1/19
+ * Time: 10:27
+ */
+
+namespace app\cost_management\model;
+
+
+use app\common\model\Base;
+
+class CostCategoryList extends Base
+{
+
+}
