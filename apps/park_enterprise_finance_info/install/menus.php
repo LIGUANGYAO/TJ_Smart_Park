2d442eae5568eb3bd2d6dadcd2d2dbb18f785461
xpwsg <xpwsgg@163.com>
2019-01-22 17:09:30 +0800

    修复菜单名称不正确的问题

--- apps/park_enterprise_finance_info/install/menus.php
+++ apps/park_enterprise_finance_info/install/menus.php
@@ -13,19 +13,19 @@ return [
             'is_menu' => 1,
             'sub_menu' => [
                 [
-                    'title' => '科技项目资金列表',
+                    'title' => '财务信息列表',
                     'name' => 'park_enterprise_finance_info/Finance/index',
                     'is_menu' => 1,
                     'icon' => ''
                 ],
                 [
-                    'title' => '添加科技项目',
+                    'title' => '添加信息',
                     'name' => 'park_enterprise_finance_info/Finance/edit',
                     'is_menu' => 0,
                     'icon' => ''
                 ],
                 [
-                    'title' => '删除科技项目',
+                    'title' => '删除信息',
                     'name' => 'park_enterprise_finance_info/Finance/delete',
                     'is_menu' => 0,
                     'icon' => ''
