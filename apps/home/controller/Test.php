f8f3a3011601450eceaf598197ca6336a13fdf51
xpwsg <xpwsgg@163.com>
2019-01-29 09:52:42 +0800

    修正知识产权模块安装信息

--- apps/home/controller/Test.php
+++ apps/home/controller/Test.php
@@ -13,14 +13,5 @@ use app\common\controller\Base;
 
 class Test extends Base
 {
-    public function index()
-    {
-        $keyword = \input('keyword');
-        $data = hook('qichacha', $keyword, true);
-        \print_r($data);
-    }
 
-    public function up(){
-        return $this->fetch();
-    }
 }
