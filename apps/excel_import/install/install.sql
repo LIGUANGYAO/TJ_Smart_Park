6b00ac6b93486a8e43679bd82c56ee666e7d9e0c
xpwsg <xpwsgg@163.com>
2019-01-30 15:11:14 +0800

    数据表添加新字段

--- apps/excel_import/install/install.sql
+++ apps/excel_import/install/install.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 29/01/2019 16:46:35
+ Date: 30/01/2019 15:00:20
 */
 
 SET NAMES utf8mb4;
@@ -22,56 +22,61 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `qnn_cost_electric_list`;
 CREATE TABLE `qnn_cost_electric_list`  (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `enterprise_id` int(11) NOT NULL COMMENT '企业ID',
-  `enterprise_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业名称',
-  `month` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '月份',
-  `build_id` int(11) NOT NULL COMMENT '楼宇号',
-  `floor` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '楼层',
-  `room_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '房间号',
-  `area` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '房间面积',
-  `s_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租赁开始日期',
-  `e_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租赁结束日期',
-  `meter_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '水表号',
-  `days` int(10) NULL DEFAULT NULL COMMENT '计费天数',
-  `last_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上月抄见数',
-  `this_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '本月抄见数',
-  `used_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '使用掉的度数',
-  `round_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT '四舍五入用电金额',
-  `share_amount` decimal(10, 5) NULL DEFAULT NULL COMMENT '均摊金额',
-  `marks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `round_share_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT '四舍五入均摊金额',
-  `real_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT '合计金额',
-  `aki_share_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '空置均摊',
-  `round_aki_share_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT '四舍五入空置均摊',
-  `tongpu_pay` decimal(10, 2) NULL DEFAULT NULL COMMENT '同普应付四舍五入金额',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT ''ID'',
+  `enterprise_id` int(11) NULL DEFAULT NULL COMMENT ''企业ID'',
+  `enterprise_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''企业名称'',
+  `year` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT ''年份'',
+  `month` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT ''月份'',
+  `park` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT ''所在园区'',
+  `build_id` int(11) NULL DEFAULT NULL COMMENT ''楼宇号'',
+  `floor` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''楼层'',
+  `room_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''房间号'',
+  `area` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''房间面积'',
+  `s_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''租赁开始日期'',
+  `e_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''租赁结束日期'',
+  `meter_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''电表号'',
+  `days` int(10) NULL DEFAULT NULL COMMENT ''计费天数'',
+  `last_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''上月抄见数'',
+  `this_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''本月抄见数'',
+  `used_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''使用掉的度数'',
+  `used_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT ''用电金额'',
+  `round_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT ''四舍五入用电金额'',
+  `share_amount` decimal(10, 5) NULL DEFAULT NULL COMMENT ''均摊金额'',
+  `round_share_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT ''四舍五入均摊金额'',
+  `real_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT ''合计金额'',
+  `aki_share_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT ''空置均摊'',
+  `round_aki_share_amount` decimal(10, 1) NULL DEFAULT NULL COMMENT ''四舍五入空置均摊'',
+  `tongpu_pay` decimal(10, 2) NULL DEFAULT NULL COMMENT ''同普应付四舍五入金额'',
+  `marks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''备注'',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT ''创建时间'',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT ''更新时间'',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '电费导入列表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = ''电费导入列表'' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for qnn_cost_water_list
 -- ----------------------------
 DROP TABLE IF EXISTS `qnn_cost_water_list`;
 CREATE TABLE `qnn_cost_water_list`  (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `enterprise_id` int(11) NOT NULL COMMENT '企业ID',
-  `enterprise_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业名称',
-  `month` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '月份',
-  `build_id` int(11) NOT NULL COMMENT '楼宇号',
-  `floor` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '楼层',
-  `room_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '房间号',
-  `area` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '房间面积',
-  `s_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租赁开始日期',
-  `e_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租赁结束日期',
-  `meter_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '水表号',
-  `last_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上月抄见数',
-  `this_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '本月抄见数',
-  `marks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT ''ID'',
+  `enterprise_id` int(11) NULL DEFAULT NULL COMMENT ''企业ID'',
+  `enterprise_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''企业名称'',
+  `year` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT ''年份'',
+  `month` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT ''月份'',
+  `park` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT ''所属园区'',
+  `build_id` int(11) NULL DEFAULT NULL COMMENT ''楼宇号'',
+  `floor` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''楼层'',
+  `room_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''房间号'',
+  `area` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''房间面积'',
+  `s_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''租赁开始日期'',
+  `e_date` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''租赁结束日期'',
+  `meter_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''水表号'',
+  `last_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''上月抄见数'',
+  `this_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''本月抄见数'',
+  `marks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ''备注'',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT ''创建时间'',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT ''更新时间'',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '导入的列表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = ''导入的列表'' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
