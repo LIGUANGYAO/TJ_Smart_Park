2da5f880db986cacc6b67b21796397fac9231c27
xpwsg <xpwsgg@163.com>
2019-01-30 14:58:40 +0800

    水电费导入完成

--- apps/excel_import/admin/Electric.php
+++ apps/excel_import/admin/Electric.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: xpwsg
- * Date: 2019/1/29
- * Time: 16:26
+ * Date: 2019/1/30
+ * Time: 13:42
  */
 
 namespace app\excel_import\admin;
@@ -12,35 +12,21 @@ namespace app\excel_import\admin;
 use app\admin\controller\Admin;
 use app\common\builder\BuilderList;
 use app\common\layout\Iframe;
-use app\excel_import\model\CostWaterList;
+use app\excel_import\model\CostElectricList;
 use think\Db;
 
-/**
- * Class Water
- * @package app\excel_import\admin
- * 导入水费控制器
- */
-class Water extends Admin
+class Electric extends Admin
 {
-    protected $db;
     protected $buildList;
 
-    /**
-     *初始化
-     */
     public function _initialize()
     {
         parent::_initialize();
-        $this->db = Db::name('CostWaterList');
         $this->buildList = Db::name('ParkBuilding')
             ->where('status', 1)
             ->column('id,title');
     }
 
-    /**
-     * @return \app\common\layout\Content
-     * 列表
-     */
     public function index()
     {
         $import = [
@@ -49,12 +35,13 @@ class Water extends Admin
             'class' => 'btn btn-default btn-sm',
             'href' => url('import')
         ];
-        list($data_list, $total) = (new CostWaterList())->search(['keyword_condition' => 'enterprise_name',])
-            ->getListByPage([], true, 'create_time desc');
+        list($data_list, $total) = (new CostElectricList())->search(['keyword_condition' => 'enterprise_name',])->getListByPage([], true, 'create_time desc');
         $content = (new BuilderList())
             ->addTopButton('self', $import)
             ->keyListItem('id', 'ID')
             ->keyListItem('enterprise_name', '企业名称')
+            ->keyListItem('year', '年份')
+            ->keyListItem('month', '月份')
             ->keyListItem('park', '园区名称')
             ->keyListItem('build_id', '楼宇', 'array', $this->buildList)
             ->keyListItem('floor', '楼层')
@@ -62,19 +49,25 @@ class Water extends Admin
             ->keyListItem('area', '面积')
             ->keyListItem('s_date', '租赁开始日期')
             ->keyListItem('e_date', '租赁结束日期')
-            ->keyListItem('meter_number', '水表号')
+            ->keyListItem('meter_number', '电表号')
+            ->keyListItem('days', '计费天数')
             ->keyListItem('last_number', '上月抄见数')
             ->keyListItem('this_number', '本月抄见数')
-            ->keyListItem('year', '年份')
-            ->keyListItem('month', '月份')
+            ->keyListItem('used_number', '使用度数')
+            ->keyListItem('round_amount', '四舍五入用电金额')
+            ->keyListItem('share_amount', '均摊金额')
+            ->keyListItem('round_share_amount', '四舍五入均摊金额')
+            ->keyListItem('real_amount', '合计金额')
+            ->keyListItem('aki_share_amount', '空置均摊')
+            ->keyListItem('round_aki_share_amount', '四舍五入空置均摊')
+            ->keyListItem('tongpu_pay', '同普应付四舍五入金额')
             ->keyListItem('marks', '备注')
             ->keyListItem('right_button', '操作', 'btn')
-            ->addRightButton('delete', ['model' => 'CostWaterList'])
+            ->addRightButton('delete', ['model' => 'CostElectricList'])
             ->setListData($data_list)
-            ->setListPage($total,10)
+            ->setListPage($total, 10)
             ->fetch();
         return (new Iframe())
-            ->setMetaTitle('水费账单列表')
             ->search([
                 ['name' => 'keyword', 'type' => 'text', 'extra_attr' => 'placeholder="请输入企业名"',],
                 ['name' => 'build_id', 'type' => 'select', 'title' => '按楼宇', 'options' => $this->buildList],
@@ -82,47 +75,55 @@ class Water extends Admin
             ->content($content);
     }
 
-    /**
-     * @throws \Exception
-     * 导入
-     */
     public function import()
     {
-        $url = '/admin.php/excel_import/water/import.html';
-        $Data = hook('importFromTable', $url, true);
-//        \halt($Data);
-        if (!empty($Data[0])) {
-            $newData = \array_slice($Data[0], 4);
+        $url = '/admin.php/excel_import/electric/import.html';
+        $sheetData = hook('importFromTable', $url, true);
+        if (!empty($sheetData[0])) {
+            $parkName = $sheetData[0][0][0];
+            $buildName = $sheetData[0][1][10];
+            $year = $sheetData[0][1][11];
+            $month = $sheetData[0][1][12];
+            $newData = \array_slice($sheetData[0], 3);
             $sqlData = [];
             foreach ($newData as $k => $v) {
                 $sqlData[$k]['enterprise_id'] = \getEnterpriseIdByEnterpriseName($v[0]);
                 $sqlData[$k]['enterprise_name'] = $v[0];
-                $sqlData[$k]['park'] = $Data[0][0][0];
-                $sqlData[$k]['build_id'] = \getBuildIdByBuildName($Data[0][0][0], $Data[0][1][5]);
-                $sqlData[$k]['year'] = $Data[0][1][6];
-                $sqlData[$k]['month'] = $Data[0][1][7];
+                $sqlData[$k]['year'] = $year;
+                $sqlData[$k]['month'] = $month;
+                $sqlData[$k]['park'] = $parkName;
+                $sqlData[$k]['build_id'] = \getBuildIdByBuildName($parkName, $buildName);
                 $sqlData[$k]['floor'] = \substr($v[1], 2, 1);
                 $sqlData[$k]['room_number'] = \substr($v[1], 4);
                 $sqlData[$k]['area'] = $v[2];
                 $sqlData[$k]['s_date'] = \substr($v[3], 0, 10);
                 $sqlData[$k]['e_date'] = \substr($v[3], 11);
                 $sqlData[$k]['meter_number'] = $v[4];
-                $sqlData[$k]['last_number'] = $v[5];
-                $sqlData[$k]['this_number'] = $v[6];
-                $sqlData[$k]['marks'] = $v[7];
-
+                $sqlData[$k]['days'] = $v[5];
+                $sqlData[$k]['last_number'] = $v[6];
+                $sqlData[$k]['this_number'] = $v[7];
+                $sqlData[$k]['used_number'] = $v[8];
+                $sqlData[$k]['used_amount'] = $v[9];
+                $sqlData[$k]['round_amount'] = \round($v[10], 1);
+                $sqlData[$k]['share_amount'] = $v[11];
+                $sqlData[$k]['round_share_amount'] = \round($v[12], 1);
+                $sqlData[$k]['real_amount'] = $v[13];
+                $sqlData[$k]['aki_share_amount'] = $v[14];
+                $sqlData[$k]['round_aki_share_amount'] = \round($v[15], 1);
+                $sqlData[$k]['tongpu_pay'] = $v[16];
+                $sqlData[$k]['marks'] = $v[17];
             }
-            $waterModel = new CostWaterList();
+            $eleModel = new CostElectricList();
             $map = [
                 'enterprise_name' => $sqlData[0]['enterprise_name'],
                 'year' => $sqlData[0]['year'],
                 'month' => $sqlData[0]['month'],
             ];
-            $count = $waterModel::where($map)->count();
+            $count = $eleModel::where($map)->count();
             if ($count > 0) {
                 $this->error('请勿重复导入');
             } else {
-                $waterModel->saveAll($sqlData);
+                $eleModel->saveAll($sqlData);
                 $this->success('导入成功', \url('index'));
             }
         }
