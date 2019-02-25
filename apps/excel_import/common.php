70917e7c2afaadb1576265106382b628c951a2d7
xpwsg <xpwsgg@163.com>
2019-02-25 11:12:24 +0800

    新增菜单

--- apps/excel_import/common.php
+++ apps/excel_import/common.php
@@ -39,3 +39,20 @@ if (!function_exists('getEnterpriseIdByEnterpriseName')) {
         }
     }
 }
+
+/**
+ * @param $enterprise_id
+ * @return mixed
+ * 根据企业ID返回企业名称
+ */
+function getEnterpriseNameByEnterpriseId($enterprise_id)
+{
+    $name = Db::name('ParkEnterpriseQichachaBasicInfo')
+        ->where('id', 'eq', $enterprise_id)
+        ->value('enterprise_name');
+    if (empty($name)) {
+        return '未知企业';
+    } else {
+        return $name;
+    }
+}
