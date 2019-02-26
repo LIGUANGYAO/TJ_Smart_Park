950d6dabf39666bde455c9e64493d3c4449aac03
xpwsg <xpwsgg@163.com>
2019-02-26 17:24:02 +0800

    逻辑层添加计算余额js代码

--- apps/park_project_budget/logic/Example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// park_project_budget模块逻辑层
-
-namespace app\park_project_budget\logic;
-use app\common\logic\Base as BaseLogic;
-
-class Example extends BaseLogic {
-
-}
---
+++ apps/park_project_budget/logic/ParkProjectBudget.php
@@ -0,0 +1,38 @@
+<?php
+// park_project_budget模块逻辑层
+
+namespace app\park_project_budget\logic;
+
+use app\common\logic\Base as BaseLogic;
+
+class ParkProjectBudget extends BaseLogic
+{
+    public function formExtraHtml()
+    {
+        return <<<EOF
+            <script type="text/javascript">
+                $(function() {
+                  $("input").keyup(function() {
+                   sum();
+                  })
+                });
+                function sum() {
+                  var num9=$("[name=amount]").val();
+                   var num10=$("[name=material_cost]").val();
+                   var num11=$("[name=test_processing_cost]").val();
+                   var num12=$("[name=fuel_cost]").val();
+                   var num13=$("[name=business_cost]").val();
+                   var num14=$("[name=knowledge_cost]").val();
+                   var num15=$("[name=service_cost]").val();
+                   var num16=$("[name=advisory_cost]").val();
+                   var num17=$("[name=other_cost]").val();
+                   var sum = parseInt(num9)-parseInt(num10)-parseInt(num11)-parseInt(num12)-parseInt(num13)-parseInt(num14)-parseInt(num15)-parseInt(num16)-parseInt(num17);
+                   $("[name=balance]").css('color','red').val(sum);
+                }
+               
+                
+            </script>
+EOF;
+
+    }
+}
