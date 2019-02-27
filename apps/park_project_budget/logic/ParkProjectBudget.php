6dfbda2634ba7e27e04393e78c93cdcbb73580f1
xpwsg <xpwsgg@163.com>
2019-02-27 15:33:42 +0800

    利用math.js进行浮点计算,解决不精确的问题

--- apps/park_project_budget/logic/ParkProjectBudget.php
+++ apps/park_project_budget/logic/ParkProjectBudget.php
@@ -19,6 +19,7 @@ class ParkProjectBudget extends BaseLogic
     public function formExtraHtml()
     {
         return <<<EOF
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/3.16.0/math.min.js"></script>
             <script type="text/javascript">
                 $(function() {
                   $("input").keyup(function() {
@@ -27,17 +28,20 @@ class ParkProjectBudget extends BaseLogic
                 });
                 function sum() {
                    var num9=$("[name=amount]").val();
-                   var num10=$("[name=material_cost]").val();
-                   var num11=$("[name=test_processing_cost]").val();
-                   var num12=$("[name=fuel_cost]").val();
-                   var num13=$("[name=business_cost]").val();
-                   var num14=$("[name=knowledge_cost]").val();
-                   var num15=$("[name=service_cost]").val();
-                   var num16=$("[name=advisory_cost]").val();
-                   var num17=$("[name=other_cost]").val();
-                   //var sum = parseInt(num9)-parseInt(num10)-parseInt(num11)-parseInt(num12)-parseInt(num13)-parseInt(num14)-parseInt(num15)-parseInt(num16)-parseInt(num17);
-                   var sum = num9-num10-num11-num12-num13-num14-num15-num16-num17;
-                   $("[name=balance]").css('color','red').val(sum);
+                   var num10=parseFloat($("[name=material_cost]").val()).toFixed(2);
+                   var num11=parseFloat($("[name=test_processing_cost]").val()).toFixed(2);
+                   var num12=parseFloat($("[name=fuel_cost]").val()).toFixed(2);
+                   var num13=parseFloat($("[name=business_cost]").val()).toFixed(2);
+                   var num14=parseFloat($("[name=knowledge_cost]").val()).toFixed(2);
+                   var num15=parseFloat($("[name=service_cost]").val()).toFixed(2);
+                   var num16=parseFloat($("[name=advisory_cost]").val()).toFixed(2);
+                   var num17=parseFloat($("[name=other_cost]").val()).toFixed(2);
+                   var num18 = parseFloat($("[name=equipment_cost]").val()).toFixed(2);
+                   var reslut = math.format(math.chain(math.bignumber(num9)).subtract(math.bignumber(num10)).subtract(math.bignumber(num11)).subtract(math.bignumber(num12)).subtract(math.bignumber(num13)).subtract(math.bignumber(num14)).subtract(math.bignumber(num15)).subtract(math.bignumber(num16)).subtract(math.bignumber(num17)).subtract(math.bignumber(num18)).done());
+                   if (isNaN(reslut)){
+                       reslut=0;
+                   } 
+                   $("[name=balance]").css('color','red').val(reslut);
                 };
             </script>
 EOF;
