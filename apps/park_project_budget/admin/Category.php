37304d6acd080ea75b157610aec034437f1ef6a5
xpwsg <xpwsgg@163.com>
2019-02-27 10:30:10 +0800

    可自行添加预算/项目种类

---
+++ apps/park_project_budget/admin/Category.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xpwsg
+ * Date: 2019/2/27
+ * Time: 9:38
+ */
+
+namespace app\park_project_budget\admin;
+
+
+use app\admin\controller\Admin;
+use app\common\builder\BuilderForm;
+use app\common\builder\BuilderList;
+use app\common\layout\Iframe;
+use app\park_project_budget\model\ParkProjectBudgetCategory;
+
+/**
+ * Class Category
+ * @package app\park_project_budget\admin
+ *项目预算种类控制器
+ */
+class Category extends Admin
+{
+    /**
+     * @var
+     * 种类模型
+     */
+    protected $cateModel;
+
+    /**
+     *
+     */
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->cateModel = new ParkProjectBudgetCategory();
+    }
+
+    /**
+     * @return \app\common\layout\Content
+     * 列表页
+     */
+    public function index()
+    {
+        list($data_list, $total) = $this->cateModel->search()->getListByPage([], true, 'create_time desc');
+        $content = (new BuilderList())
+            ->addTopButton('addnew')
+            ->addTopButton('delete')
+            ->keyListItem('id', 'ID')
+            ->keyListItem('type', '类别', 'array', [1 => '项目', 2 => '预算'])
+            ->keyListItem('name', '名称')
+            ->keyListItem('right_button', '操作', 'btn')
+            ->addRightButton('edit')
+            ->addRightButton('delete', ['model' => 'ParkProjectBudgetCategory'])
+            ->setListData($data_list)
+            ->setListPage($total)
+            ->fetch();
+        return (new Iframe())
+            ->setMetaTitle('类别列表')
+            ->content($content);
+    }
+
+    /**
+     * @param int $id
+     * @return \app\common\layout\Content
+     * @throws \think\exception\DbException
+     * 编辑/新增种类
+     */
+    public function edit($id = 0)
+    {
+        $title = $id > 0 ? '编辑' : '新增';
+        if (IS_POST) {
+            $param = \input();
+            if ($this->cateModel->editData($param)) {
+                $this->success($title . '成功', \url('index'));
+            } else {
+                $this->error($this->cateModel->getError());
+            }
+        } else {
+            $info = ['type' => 2];
+            if ($id > 0) {
+                $info = ParkProjectBudgetCategory::get($id);
+            }
+            $content = (new BuilderForm())
+                ->addFormItem('id', 'hidden', 'ID')
+                ->addFormItem('type', 'radio', '类别', '', [1 => '项目', 2 => '预算'])
+                ->addFormItem('name', 'text', '名称')
+                ->setFormData($info)
+                ->addButton('submit')
+                ->addButton('back')
+                ->fetch();
+            return (new Iframe())
+                ->setMetaTitle($title . '类别')
+                ->content($content);
+        }
+    }
+}
---
+++ apps/park_project_budget/model/ParkProjectBudgetCategory.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xpwsg
+ * Date: 2019/2/27
+ * Time: 9:47
+ */
+
+namespace app\park_project_budget\model;
+
+
+use app\common\model\Base;
+
+class ParkProjectBudgetCategory extends Base
+{
+
+}
