bb793a9002a9d22ce5fc444324f60c0510f5dd54
xpwsg <xpwsgg@163.com>
2019-02-28 14:31:23 +0800

    添加注释

--- apps/park_project_budget/model/ParkProjectBudgetList.php
+++ apps/park_project_budget/model/ParkProjectBudgetList.php
@@ -12,8 +12,18 @@ namespace app\park_project_budget\model;
 use app\common\model\Base;
 use think\Db;
 
+/**
+ * Class ParkProjectBudgetList
+ * @package app\park_project_budget\model
+ * 项目列表模型层
+ */
 class ParkProjectBudgetList extends Base
 {
+    /**
+     * @param $value
+     * @return mixed
+     * 返回type中文名
+     */
     public function getTypeAttr($value)
     {
         $type = Db::name('ParkProjectBudgetCategory')
@@ -22,6 +32,11 @@ class ParkProjectBudgetList extends Base
         return $type[$value];
     }
 
+    /**
+     * @param $value
+     * @return mixed
+     * 返回状态中文名
+     */
     public function getProjectStatusAttr($value)
     {
         $project_status = [1 => '未执行', 2 => '执行中', 3 => '执行完毕'];
