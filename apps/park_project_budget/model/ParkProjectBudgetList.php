b222a0e2eeaa8cff2359e184a4dd29f0ff819ac5
xpwsg <xpwsgg@163.com>
2019-02-28 14:27:31 +0800

    新增两个字段的获取器

--- apps/park_project_budget/model/ParkProjectBudgetList.php
+++ apps/park_project_budget/model/ParkProjectBudgetList.php
@@ -10,8 +10,21 @@ namespace app\park_project_budget\model;
 
 
 use app\common\model\Base;
+use think\Db;
 
 class ParkProjectBudgetList extends Base
 {
+    public function getTypeAttr($value)
+    {
+        $type = Db::name('ParkProjectBudgetCategory')
+            ->where('type', 'eq', 1)
+            ->column('id,name');;
+        return $type[$value];
+    }
 
+    public function getProjectStatusAttr($value)
+    {
+        $project_status = [1 => '未执行', 2 => '执行中', 3 => '执行完毕'];
+        return $project_status[$value];
+    }
 }
